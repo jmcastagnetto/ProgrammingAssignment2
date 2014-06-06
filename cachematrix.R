17527ed0f2b910b5fdaa7d2a5462efe739a9846d
Jesus M. Castagnetto <jesus@upch.pe>
2014-06-05 22:34:42 -0500

    solution to the peer assessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+# A function that defines an special "matrix" that can cache its inverse
+# and another that calculates returns the inverse of the special "matrix"
 
-## Write a short comment describing this function
+# Defines a special "matrix" capable of caching its own
+# inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(w) {
+        x <<- w
+        inv <<- NULL
+    }
+    get <- function() {
+        return(x)
+    }
+    setinverse <- function(inverse) {
+        inv <<- inverse
+    }
+    getinverse <- function() {
+        return(inv)
+    }
+    list(get=get, set=set,
+         setinverse=setinverse,
+         getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# returns the inverse of an special "matrix" object
+# indicating whether the result has been cached
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if (!is.null(inv)) {
+        message("getting the cached value of the inverse of the matrix")
+        return(inv)
+    } else {
+        mat <- x$get()
+        inv <- solve(mat)
+        x$setinverse(inv)
+        return(inv)
+    }
 }
+
+## Test the functions defined above
+
+set.seed(13579)
+
+# Let's create an special "matrix" from a 3x3 matrix
+mat <- matrix(sample(1:4,9,replace=TRUE), nrow=3, ncol=3)
+cmat <- makeCacheMatrix(mat)
+invcmat <- cacheSolve(cmat)
+# get the cached value
+cacheSolve(cmat)
+# compare the inverse with the 3x3 unit matrix
+all.equal(round(invcmat %*% mat,1), diag(3))
+
+# now, we will use a 100x100 matrix
+mat2 <- matrix(sample(1:4, 10000,replace=TRUE), nrow=100, ncol=100)
+cmat2 <- makeCacheMatrix(mat2)
+invcmat2 <- cacheSolve(cmat2)
+# get the cached value
+tmp <- cacheSolve(cmat2)
+# compare with the 100x100 identity matrix
+all.equal(round(tmp %*% mat2, 1), diag(100))
+
---
+++ vector-functions-example.R
@@ -0,0 +1,32 @@
+# vector functions example
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
+
+# test example code
+set.seed(12345)
+vec <- makeVector(sample(1:20, 7))
+cachemean(vec)
+cachemean(vec)
